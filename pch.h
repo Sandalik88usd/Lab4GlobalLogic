23200fde2f348b776ee1e09a8ec9bed6b9782961
Serhii <119784654+Sandalik88usd@users.noreply.github.com>
2025-05-19 16:48:04 +0300

    lab4

---
+++ pch.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Basic C headers (використовуємо .h замість c++)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+// Google Test
+#include <gtest/gtest.h>
